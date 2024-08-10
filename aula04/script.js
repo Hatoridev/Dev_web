1d47b2ec2cc068c41586cc8106cace31ba47752d
Hatoridev <jonasdev021@outlook.com>
2024-08-10 10:51:05 -0300

    Adicionar arquivos da aula04 de Desenvolvimento Web usando HTML, CSS e JavaScript

---
+++ aula04/script.js
@@ -0,0 +1,9 @@
+// Exemplo básico de script JavaScript
+console.log('Página carregada com sucesso');
+
+// Comentários:
+
+// 1. Este script é um exemplo básico que simplesmente registra uma mensagem no console quando a página é carregada.
+
+// 2. Você pode adicionar funcionalidades JavaScript adicionais aqui, como interações dinâmicas com a página ou validação de formulários.
+
