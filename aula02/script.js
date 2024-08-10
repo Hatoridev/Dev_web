43c407b9053c4d8e5eebf22952d37b7cbb70041e
Hatoridev <jonasdev021@outlook.com>
2024-08-10 10:50:33 -0300

    Adicionar arquivos da aula02 de Desenvolvimento Web usando HTML, CSS e JavaScript

---
+++ aula02/script.js
@@ -0,0 +1,16 @@
+// Função para adicionar produtos ao carrinho
+function addToCart(productName) {
+    alert(productName + ' foi adicionado ao carrinho!');
+    // Exibe um alerta informando que o produto foi adicionado ao carrinho
+    // O nome do produto é passado como argumento e incluído na mensagem do alerta
+}
+/* 
+    Comentários:
+
+    - Define a função `addToCart` que é chamada quando um botão de produto é clicado.
+
+    - A função exibe uma mensagem de alerta indicando que o produto foi adicionado ao carrinho.
+
+    - O nome do produto é passado para a função via atributo `onclick` nos botões no HTML.
+*/
+
