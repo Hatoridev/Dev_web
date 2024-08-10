b3bcf8fa2e5dc979c342dca9ecfd2fc1e8109da6
Hatoridev <jonasdev021@outlook.com>
2024-08-10 10:50:49 -0300

    Adicionar arquivos da aula03 de Desenvolvimento Web usando HTML, CSS e JavaScript

---
+++ aula03/script.js
@@ -0,0 +1,13 @@
+// Exemplo básico de script JavaScript, pode adicionar funcionalidades aqui
+console.log('Página carregada com sucesso');
+
+/*
+Comentário:
+
+Este arquivo JavaScript é um espaço para adicionar funcionalidades interativas.
+
+- No momento, ele apenas exibe uma mensagem no console quando a página é carregada.
+
+- Você pode adicionar código aqui para funcionalidades como validação de formulário, interações com o usuário, etc.
+*/
+
